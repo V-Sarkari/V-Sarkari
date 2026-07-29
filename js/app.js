070653e02800a3eba9ee4223ef0b55e3afeee84f
V-Sarkari <officialvsarkari@gmail.com>
2026-07-29 18:37:37 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -48,3 +48,34 @@ container.innerHTML += `
 });
 
 });
+// Smart Search
+
+const searchInput = document.getElementById("searchInput");
+
+if (searchInput) {
+
+searchInput.addEventListener("keyup", function () {
+
+const value = this.value.toLowerCase();
+
+const cards = document.querySelectorAll(".job-card");
+
+cards.forEach(card => {
+
+const text = card.innerText.toLowerCase();
+
+if (text.includes(value)) {
+
+card.style.display = "block";
+
+} else {
+
+card.style.display = "none";
+
+}
+
+});
+
+});
+
+}
