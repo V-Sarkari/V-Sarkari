143ad9846a8781723127411fe8c6155a2fa2991f
V-Sarkari <officialvsarkari@gmail.com>
2026-07-29 18:25:52 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -11,3 +11,40 @@ if (darkBtn) {
         document.body.classList.toggle("dark-mode");
     });
 }
+// Dynamic Job Loader
+
+fetch("data/jobs.json")
+.then(response => response.json())
+.then(jobs => {
+
+const container = document.getElementById("jobsContainer");
+
+if(!container) return;
+
+jobs.forEach(job=>{
+
+container.innerHTML += `
+
+<div class="job-card">
+
+<h3>${job.title}</h3>
+
+<p>${job.department}</p>
+
+<p><b>Qualification:</b> ${job.qualification}</p>
+
+<p><b>Last Date:</b> ${job.lastDate}</p>
+
+<a href="job-details.html">
+
+<button>${job.status}</button>
+
+</a>
+
+</div>
+
+`;
+
+});
+
+});
