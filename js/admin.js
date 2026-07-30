1149ce5988f5ddb1a6e3e2eddabbdd3a6097bea4
V-Sarkari <officialvsarkari@gmail.com>
2026-07-30 10:30:55 +0530

    admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,24 @@
-console.log("Admin JS Loaded");
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+const loginBtn = document.getElementById("googleLogin");
+const logoutBtn = document.getElementById("googleLogout");
+
+loginBtn.onclick = () => {
+  firebase.auth().signInWithPopup(provider);
+};
+
+logoutBtn.onclick = () => {
+  firebase.auth().signOut();
+};
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    loginBtn.style.display = "none";
+    logoutBtn.style.display = "inline-block";
+    console.log("Logged in:", user.email);
+  } else {
+    loginBtn.style.display = "inline-block";
+    logoutBtn.style.display = "none";
+  }
+});
